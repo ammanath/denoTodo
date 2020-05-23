2dc7d1cd2b5f9c5a7e5f4f3e09c8305958ca2f52
bit00 <akbrijesh@gmail.com>
2020-05-23 21:44:35 +0530

    minor get change

--- controllers/todos/get.js
+++ controllers/todos/get.js
@@ -1,11 +1,16 @@
 import {FILE_PATH} from '../../config.js';
 
 export default  async ({ response }) => {
+    const decoder = new TextDecoder();
     try{
     const data = await Deno.readFile(FILE_PATH);
-    console.log(data);
+    const todos = JSON.parse(decoder.decode(data));
+    console.log(todos);
+    response.status = 200;
+    response.body = {status:'success', todos}
     }catch(error){
-
+        response.status = 500;
+        response.body = {status:'failes', todos:[]}
     }
     
   }
